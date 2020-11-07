6e69963914789f0c9a67ebf7a8cfff4ab24b40f2
Combatd <combatdstar@gmail.com>
2020-11-07 13:11:49 -0800

    Define #compress_str(sttr)

--- compress_string.rb
+++ compress_string.rb
@@ -1,7 +1,19 @@
+# compress string into lowercase string containing the character next to the times it appears in the string
+# return the original string if the compressed string is longer than the original
 def compress_string(str)
-    compressed_string = "" # initialize empty string
-end
+    compressed_string = ""
+    str.each_char do |char, idx|
+        count = letter_count(str, char)
+        compressed_string += "#{char}#{count}"
+    end
 
+    if compressed_string.length > str.length
+        return str
+    else
+        return compressed_string
+    end
+end
+# keeping the letter count separate works well when we don't have many characters
 def letter_count(string, char)
     count = 0 # initialize count with 0
     # downcase for ease of comparisons
@@ -14,3 +26,5 @@ def letter_count(string, char)
     end
     count
 end
+
+puts compress_string("racecar")
